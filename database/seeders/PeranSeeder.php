fc02a6c7c42c41fad9f25706c7456296b38d1ebd
fatihur <fatihur17@gmail.com>
2025-12-13 00:35:47 +0800

    update seeder lagi

--- database/seeders/PeranSeeder.php
+++ database/seeders/PeranSeeder.php
@@ -17,15 +17,15 @@ class PeranSeeder extends Seeder
             'deskripsi' => 'Super Administrator dengan akses penuh'
         ]);
 
-        $guru = Peran::create([
-            'nama_peran' => 'guru',
-            'deskripsi' => 'Guru yang mengelola materi dan kuis'
-        ]);
+        // $guru = Peran::create([
+        //     'nama_peran' => 'guru',
+        //     'deskripsi' => 'Guru yang mengelola materi dan kuis'
+        // ]);
 
-        $siswa = Peran::create([
-            'nama_peran' => 'siswa',
-            'deskripsi' => 'Siswa yang mengakses materi dan mengerjakan kuis'
-        ]);
+        // $siswa = Peran::create([
+        //     'nama_peran' => 'siswa',
+        //     'deskripsi' => 'Siswa yang mengakses materi dan mengerjakan kuis'
+        // ]);
 
         // Buat user default
         User::create([
@@ -36,27 +36,27 @@ class PeranSeeder extends Seeder
             'email_verified_at' => now()
         ]);
 
-        User::create([
-            'name' => 'Guru Demo',
-            'email' => 'guru@belajaringgris.com',
-            'password' => Hash::make('guru123'),
-            'peran_id' => $guru->id,
-            'nomor_induk' => '198501012010011001',
-            'no_telepon' => '081234567890',
-            'jenis_kelamin' => 'L',
-            'email_verified_at' => now()
-        ]);
+        // User::create([
+        //     'name' => 'Guru Demo',
+        //     'email' => 'guru@belajaringgris.com',
+        //     'password' => Hash::make('guru123'),
+        //     'peran_id' => $guru->id,
+        //     'nomor_induk' => '198501012010011001',
+        //     'no_telepon' => '081234567890',
+        //     'jenis_kelamin' => 'L',
+        //     'email_verified_at' => now()
+        // ]);
 
-        User::create([
-            'name' => 'Siswa Demo',
-            'email' => 'siswa@belajaringgris.com',
-            'password' => Hash::make('siswa123'),
-            'peran_id' => $siswa->id,
-            'nomor_induk' => '2024001',
-            'kelas' => 'X-A',
-            'no_telepon' => '081234567891',
-            'jenis_kelamin' => 'P',
-            'email_verified_at' => now()
-        ]);
+        // User::create([
+        //     'name' => 'Siswa Demo',
+        //     'email' => 'siswa@belajaringgris.com',
+        //     'password' => Hash::make('siswa123'),
+        //     'peran_id' => $siswa->id,
+        //     'nomor_induk' => '2024001',
+        //     'kelas' => 'X-A',
+        //     'no_telepon' => '081234567891',
+        //     'jenis_kelamin' => 'P',
+        //     'email_verified_at' => now()
+        // ]);
     }
 }
